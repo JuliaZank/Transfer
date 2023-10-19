4a9f715652638a7c1178ee49ce6d83689a634478
Neto test <zanckeeva2017@yandex.ru>
2023-10-19 22:30:02 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val commission = calculatePrice("Mastercard", 0, 100000)
+    println(commission)
+}
+
+fun calculatePrice(typeCard: String, totalAmount: Int, amountTransfer: Int): String {
+    val total = userTypeCard(typeCard) * amountTransfer
+
+    var commissionMM =
+        if (amountTransfer > 150_000) {
+            "Сумма перевода не должна превышать 150_000 рублей"
+        } else if ((typeCard == "Mastercard" || typeCard == "Maestro") && amountTransfer < 75_000) {
+            "Комиссия составит 0 рублей"
+        } else "Комиссия составит ${total + 20} рублей"
+    val commissionVM =
+        if (amountTransfer > 150_000) {
+            "Сумма перевода не должна превышать 150_000 рублей"
+        } else if ((typeCard == "Visa" || typeCard == "Мир") && total < 35) {
+            "Комиссия составит 35 рублей"
+        } else "Комиссия составит $total рублей"
+    val commissionVK =
+        if (typeCard == "VK Pay" && amountTransfer > 15_000) "Сумма перевода не должна превышать 15_000 рублей" else "Комиссия составит $total рублей"
+
+    val end = when (typeCard) {
+        "Mastercard" -> commissionMM
+        "Maestro" -> commissionMM
+        "Visa" -> commissionVM
+        "Мир" -> commissionVM
+        else -> commissionVK
+    }
+    return end
+}
+
+fun userTypeCard(typeCard: String) = when (typeCard) {
+    "Mastercard" -> 0.6
+    "Maestro" -> 0.6
+    "Visa" -> 0.75
+    "Мир" -> 0.75
+    else -> 0.0
+}
